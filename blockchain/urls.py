d74965dfa5aeae221af156e21cc3f0a0f82cd419
giridhar <giridhar7889@gmail.com>
2021-07-11 18:54:09 +0530

    static transactions

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('get_chain/', views.get_chain, name="get_chain"),
     path('mine_block/', views.mine_block, name="mine_block"),
     path('is_valid/', views.is_valid, name="is_valid"),
-]
+    path('add_transaction/', views.add_transaction, name="add_transaction"),
+    path('connect_node/', views.connect_node, name="connect_node"),
+    path('replace_chain/', views.replace_chain, name="replace_chain")
+    ]
--- blockchain/views.py
+++ blockchain/views.py
@@ -1,3 +1,4 @@
+from django.contrib.sites import requests
 from django.shortcuts import render
 import datetime
 import hashlib
@@ -7,24 +8,27 @@ from uuid import uuid4
 import socket
 from urllib.parse import urlparse
 from django.http import JsonResponse, HttpResponse, HttpRequest
-from django.views.decorators.csrf import csrf_exempt #New
+from django.views.decorators.csrf import csrf_exempt  # New
 
 
 class Blockchain:
 
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(nonce=1, previous_hash='0')
-        self.transactions=[]
 
+        self.nodes = []
 
     def create_block(self, nonce, previous_hash):
         block = {'index': len(self.chain) + 1, 'timestamp': str(datetime.datetime.now()),
-                 'nonce': nonce, 'previous_hash': previous_hash}
+                 'nonce': nonce, 'previous_hash': previous_hash, 'transactions': self.transactions}
+        self.transactions = []
         self.chain.append(block)
+
         return block
 
-    def get_previous_block(self):
+    def get_last_block(self):
         return self.chain[-1]
 
     def proof_of_work(self, previous_hash):
@@ -61,26 +65,66 @@ class Blockchain:
 
         return True
 
+    def add_transaction(self, sender, receiver, amount, time):
+        self.transactions.append({
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount,
+            'time': str(datetime.datetime.now())
+        })
+        previous_block = self.get_last_block()
+        return previous_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
 
 # creating a blockchain
 
 blockchain = Blockchain()
 
 
-# mining a blockchain
+# creating an address for the node running on the server
+node_address = str(uuid4()).replace('-', '') #New
+root_node = 'e36f0158f0aed45b3bc755dc52ed4560d'
 
+# mining a blockchain
 def mine_block(request):
     if request.method == "GET":
-        previous_block = blockchain.get_previous_block()
+        previous_block = blockchain.get_last_block()
         previous_nonce = previous_block['nonce']
         nonce = blockchain.proof_of_work(previous_nonce)
         previous_hash = blockchain.hash(previous_block)
+
+        blockchain.add_transaction(sender=root_node,receiver=node_address, amount=7.1,
+                                   time=str(datetime.datetime.now()))
         block = blockchain.create_block(nonce, previous_hash)
         response = {'message': 'Congratulations, you just mined a block!',
                     'index': block['index'],
                     'timestamp': block['timestamp'],
                     'nonce': block['nonce'],
-                    'previous_hash': block['previous_hash']}
+                    'previous_hash': block['previous_hash'],
+                    'transactions':block['transactions']
+
+                    }
 
         return JsonResponse(response)
 
@@ -103,4 +147,43 @@ def is_valid(request):
             response = {'message': 'All good. The Blockchain is valid.'}
         else:
             response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+    return JsonResponse(response)
+
+#adding new transactions to the blockchain
+
+@csrf_exempt
+def add_transaction(request): #New
+    if request.method == 'POST':
+        received_json = json.loads(request.body)
+        transaction_keys = ['sender', 'receiver', 'amount','time']
+        if not all(key in received_json for key in transaction_keys):
+            return 'Some elements of the transaction are missing', HttpResponse(status=400)
+        index = blockchain.add_transaction(received_json['sender'], received_json['receiver'], received_json['amount'],received_json['time'])
+        response = {'message': f'This transaction will be added to Block {index}'}
+    return JsonResponse(response)
+
+# Connecting new nodes
+@csrf_exempt
+def connect_node(request): #New
+    if request.method == 'POST':
+        received_json = json.loads(request.body)
+        nodes = received_json.get('nodes')
+        if nodes is None:
+            return "No node", HttpResponse(status=400)
+        for node in nodes:
+            blockchain.add_node(node)
+        response = {'message': 'All the nodes are now connected. The town now contains the following nodes:',
+                    'total_nodes': list(blockchain.nodes)}
+    return JsonResponse(response)
+
+# Replacing the chain by the longest chain if needed
+def replace_chain(request): #New
+    if request.method == 'GET':
+        is_chain_replaced = blockchain.replace_chain()
+        if is_chain_replaced:
+            response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
+                        'new_chain': blockchain.chain}
+        else:
+            response = {'message': 'All good. The chain is the largest one.',
+                        'actual_chain': blockchain.chain}
     return JsonResponse(response)
