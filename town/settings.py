4f20f13b206d9a3c07732cd605ba87e2dc2e5159
giridhar <giridhar7889@gmail.com>
2021-07-12 23:27:59 +0530

    configured mysql

--- town/settings.py
+++ town/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = 'town.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'Giridhar2339',
+        'NAME': 'town',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
